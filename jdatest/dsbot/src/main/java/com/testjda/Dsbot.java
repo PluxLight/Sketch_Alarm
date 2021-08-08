78fe92462a44247b14290d3c45f05926d0fefb58
PluxLight <aeoragy@gmail.com>
2021-08-09 00:03:37 +0900

    create
    testbot build

---
+++ jdatest/dsbot/src/main/java/com/testjda/Dsbot.java
@@ -0,0 +1,16 @@
+package com.testjda;
+
+// import org.apache.commons.lang3.StringUtils;
+
+/**
+ * Hello world!
+ *
+ */
+public class Dsbot 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+        // System.out.println(StringUtils.capitalize("hello maven world"));
+    }
+}
