f315439ecc978912c0be4ea5ed8316d1727bce95
PluxLight <aeoragy@gmail.com>
2021-08-11 13:38:44 +0900

    Update winalarm.py
    완성

--- pywinnoti/winalarm.py
+++ pywinnoti/winalarm.py
@@ -10,22 +10,27 @@ import time
 import json
 
 import sys
+import os
 
 #쿠키 값 외부에서 읽어들이는 함수 생성하고 나머지 마무리
 #스케줄러로 1분마다 실행되게끔
+#n회차 크롤링 시작 종료 알림
 
 class WinAlarm():
 
     
     def __init__(self):
+
+        self.title = "방송 시작 알림"
+        self.contents = "님이 방송 중입니다."
+
         self.check_cookie_page = 'https://www.pixiv.net/'
         self.check_user_page = 'https://sketch.pixiv.net/@user_name'  #https://sketch.pixiv.net/@'user_name'
         self.live_page = 'https://sketch.pixiv.net' #https://sketch.pixiv.net/@'user_name'/lives/'ch_id'
+        self.path_icon = 'icon.ico'
+        self.crawl_count = 1
 
-
-        self.pixiv_cookie = {'PHPSESSID' : '7937790_pPfJxt7dIt26vd7Z3zFEp0c3urZcx8Um'} #이게 진짜 값
-        # pixiv_cookie = {'PHPSESSID' : '7937790_pPfJxt7dIt26vd7Z3zFEp0c3urZcx8a'} #테스트용 가짜 값
-        #실제로는 cookie.json에서 읽어와야함
+        self.pixiv_cookie = {'PHPSESSID' : ''} #Cookie.json에서 값을 읽어온다
         self.userAgent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.277 Whale/2.9.118.38 Safari/537.36"
         self.user_header = {
                             'user-agent' : self.userAgent, 
@@ -35,6 +40,18 @@ class WinAlarm():
                             'accept-encoding' : 'gzip, deflate, br',
                             'accept-language' : 'ko-KR,ko;q=0.9,en-US;q=0.8,en;q=0.7',
                             }
+
+
+    def read_coookie(self):
+        
+        try:
+            with open('Cookie.json', 'r') as file:
+                self.pixiv_cookie = json.load(file)
+
+        except Exception as e:
+            print(e + '\n')
+            print('Cookie.json 파일을 읽는데 실패했습니다.')
+            self.end_program()
 
 
     def check_cookie(self):
@@ -50,13 +67,12 @@ class WinAlarm():
 
                 else:
                     print("픽시브 로그인 실패. 쿠키값을 갱신하고 다시 실행해주세요.") #콘솔에서 보일거
-                    sys.exit('program end...')
-                    #알람 메세지로도 보이게끔
+                    self.end_program()
 
-        except:
-            print("픽시브 로그인 실패. 쿠키값을 갱신하고 다시 실행해주세요.") #콘솔에서 보일거
-            sys.exit('program end...')
-            #알람 메세지로도 보이게끔
+        except Exception as e:
+            print(e + '\n')
+            print("픽시브 접속 실패. 쿠키값을 갱신하거나 인터넷 상황을 확인하세요.")
+            self.end_program()
 
 
     def clear_artist_status(self):
@@ -64,35 +80,41 @@ class WinAlarm():
         self.artists = []
         self.artists_status = dict()
         cnt = 1
-        
-        f = open('user.txt', 'r', encoding='utf8')
-        while True:
-            line = f.readline().strip()
 
-            if not line: #더이상 읽을 줄이 없거나 줄이 있어도 공백이면
-                break
-            self.artists.append(line)
-            self.artists_status[line] = 'False'
-        f.close()
+        try:
+            with open('user.txt', 'r', encoding='utf8') as file:
+                 while True:
+                    line = file.readline().strip()
+
+                    if not line: #더이상 읽을 줄이 없거나 줄이 있어도 공백이면
+                        break
+                    self.artists.append(line)
+                    self.artists_status[line] = 'False'
+
+        except Exception as e:
+            print(e + '\n')
+            print('user.txt를 읽지 못했습니다.\nuser.txt 파일이 실행 프로그램과 같은 폴더에 있는지 확인하세요.')
+            self.end_program()
+
         print(f'현재 등록된 작가는 {len(self.artists)}명이며 다음과 같습니다.')
 
         for artist in self.artists:
             print(f'{cnt}. {artist}')
             cnt += 1
+
         print('\n')
-        # print()
-        # for arts in self.artist_status.keys():
-        #     print(f'key={arts} value={self.artist_status[arts]}')
         
 
     def crawl(self):
 
         if len(self.artists) == 0:
             print(f'등록한 작가가 없습니다. user.txt 파일에 한 줄에 한명씩 작가를 입력해주세요.')
+            self.end_program()
 
         else:
+            print(f'\n{self.crawl_count}번째 크롤링을 시작합니다.\n')
 
-            for self.artist in self.artists: #self.check_user_page = 'https://sketch.pixiv.net/@user_name'
+            for self.artist in self.artists:
                 split_url = self.check_user_page.split('@')[0]
                 self.check_user_page = split_url + '@' + self.artist
 
@@ -103,96 +125,106 @@ class WinAlarm():
                         html = req.text
                         
                         if 'Live broadcasting' in html and self.artists_status[self.artist] == 'False':
-                            print(f'{self.artist}님이 방송중입니다.')
-                            self.artists_status[self.artist] == 'True'
                             soup = BeautifulSoup(html, 'html.parser')
+                            self.nick = soup.select('div.UserHeaderBody > div > div.user > div.name')
+                            self.nick = self.nick[0].text
+                            self.artists_status[self.artist] = 'True'
                             self.ch_id = soup.select('div > div.UserHeaderBody > div > div.live-button > a')
-                            # print(ch_id)
                             self.ch_id = self.ch_id[0].get('href')
 
-                            print(f'URL = {self.live_page}{self.ch_id}')
-                            self.notice()
+                            print(f'\n{self.nick}({self.artist})님이 방송중입니다.') #콘솔에서 보일거
+                            print(f'URL = {self.live_page}{self.ch_id}\n')
+
+                            self.notice(True)
+
+                        elif 'Live broadcasting' in html and self.artists_status[self.artist] == 'True':
+                            print(f'\n{self.artist}님이 방송중입니다.') #콘솔에서 보일거
+                            print(f'URL = {self.live_page}{self.ch_id}\n')
 
                         else:
                             print(f'{self.artist}님은 휴식중입니다.') #콘솔에서 보일거
-                            self.artists_status[self.artist] == 'False'
+                            self.artists_status[self.artist] = 'False'
                             #알람 메세지로도 보이게끔
 
-                except:
-                    print("픽시브 로그인 실패. 쿠키값을 갱신하고 다시 실행해주세요.") #콘솔에서 보일거
-                    sys.exit('program end...')
-                    #알람 메세지로도 보이게끔
-                time.sleep(1)
-        
-        
+                    else: #404 등의 유저 오입력 경우
+                        print("유저 정보 확인에 실패했습니다. user.txt에 입력한 유저값이 정상인지 확인하세요.") #콘솔에서 보일거
+                        self.end_program()
 
 
-    def open_url(self):
+                except Exception as e:
+                    print(e + '\n')
+                    print("유저 정보 확인에 실패했습니다. user.txt에 입력한 유저값이 정상인지 확인하세요.") #콘솔에서 보일거
+                    self.end_program()
+            
+            print(f'\n{self.crawl_count}번째 크롤링이 끝났습니다.\n1분 후 작업을 다시 시작합니다.')
+            self.crawl_count += 1
+
+
+    def click_open_url(self):
+
         live_url = self.live_page+self.ch_id
+
         try:
             webbrowser.open(live_url)
-            print('open url...')
             
-        except:
-            print('Failed open URL')
+        except Exception as e:
+            print(e + '\n')
+            print('URL을 여는데 실패했습니다.')
+            self.end_program()
 
 
-    def notice(self):
+    def notice(self, bool):
         toaster = ToastNotifier()
 
-        toaster.show_toast("라이브 시작 알림", 
-                            self.artist+"님의 방송이 시작됐습니다",
-                            icon_path=None,
-                            duration=3,
-                            threaded=False,
-                            callback_on_click=self.open_url)
+        try:
+
+            if bool == True:
+                msg = f'{self.nick}({self.artist}) {self.contents}'
+                toaster.show_toast(self.title, 
+                                    msg,
+                                    icon_path=self.path_icon,
+                                    duration=3,
+                                    threaded=False,
+                                    callback_on_click=self.click_open_url)
+
+            else:
+                toaster.show_toast(self.title, 
+                                    self.contents,
+                                    icon_path=self.path_icon,
+                                    duration=10,
+                                    threaded=False)
+
+        except  Exception as e:
+            print(e + '\n')
+            print('알림 메시지 전송에 실패했습니다.')
+            self.end_program()
 
 
-    # def schedul():
-    #     schedule.every(1).minutes.do(printtime)
-    #     while True:
-    #         schedule.run_pending()
-    #         time.sleep(1)
+
+    def end_program(self):
+        print('동일현상 중복 발생 시 개발자에게 문의 바랍니다.')
+        self.title = "프로그램 종료"
+        self.contents = "오류로 인해 프로그램을 종료합니다.\n프로그램 화면을 확인하세요"
+        self.notice(False)
+        os.system("pause")
+        sys.exit(0)
 
 
-    # def printtime(self):
-    #     print('im print')
+    def schedul(self):
+        schedule.every(1).minutes.do(self.crawl)
+        while True:
+            schedule.run_pending()
+            time.sleep(1)
+
 
     def main(self):
+        self.read_coookie()
         self.clear_artist_status()
         self.check_cookie()
         self.crawl()
-
-
-
+        self.schedul() #여기서 무한반복
 
 
 if __name__ == "__main__":
     do_class = WinAlarm()
     do_class.main()
-    
-
-# req = requests.get(visit_url, cookies=self.pixiv_cookie, headers=self.user_header)
-
-        # html = req.text
-        # header = req.headers
-        # status = req.status_code
-
-        # isok = req.ok
-
-        # soup = BeautifulSoup(html, 'html.parser')
-
-
-        # print()
-        # print()
-        # print(status, isok)
-
-        
-        # live_check = soup.select(
-        # 'div > div.live-button > a > button > span > span'
-        # )
-
-        # if live_check != []:
-        #     print('now live!')
-        # else:
-        #     print('no live')          
